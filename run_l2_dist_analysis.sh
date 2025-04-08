7629158acd20b74b340f1af440aeb36699bc4032
Yi-Chung Chen <chen5262@purdue.edu>
2025-04-07 21:22:27 -0400

    Show only overall plot with both correct and wrong cond

--- run_l2_dist_analysis.sh
+++ run_l2_dist_analysis.sh
@@ -1,6 +1,6 @@
 # CUDA_VISIBLE_DEVICES=7 python var_size_analysis.py --mode l2_dist --partial 10 --dataset imagenet10
 # CUDA_VISIBLE_DEVICES=7 python var_dist_analysis.py --mode l2_dist --partial 500 --dataset imagenet10
-CUDA_VISIBLE_DEVICES=7 python var_size_analysis.py --mode l2_dist --partial 500 --plot_dist_kde --extra kde_plot
+CUDA_VISIBLE_DEVICES=6 python var_size_analysis.py --mode l2_dist --partial 500 --plot_dist_kde
 
 # for cfg in $(seq 0.0 0.5 8.0); do
 #     CUDA_VISIBLE_DEVICES=7 python var_dist_analysis.py --mode l2_dist --partial 500 --cfg $cfg
